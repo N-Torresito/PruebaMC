de262c8385dac90572d558c175d7562a2ca46ac6
Torresito <nicolastorresroa@gmail.com>
2024-10-04 13:44:20 -0500

    Initial Commit

---
+++ src/client/java/casti/benja/PruebaMCClient.java
@@ -0,0 +1,10 @@
+package casti.benja;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PruebaMCClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
