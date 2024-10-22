a7294305953d8f57649149a1de26fa412510c573
Torresito <178304045+N-Torresito@users.noreply.github.com>
2024-10-22 16:33:09 -0500

    Added a new Item that can be eaten and has the same attributes as the cooked beef

--- src/main/java/casti/benja/items/ModItems.java
+++ src/main/java/casti/benja/items/ModItems.java
@@ -3,6 +3,8 @@ package casti.benja.items;
 import casti.benja.PruebaMC;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.component.type.FoodComponents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -13,6 +15,7 @@ public class ModItems {
 
     public static final Item PINK_GARNET  = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET  = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+    public static final Item BANANA = registerItem("banana", new Item(new Item.Settings().food(FoodComponents.COOKED_BEEF)));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(PruebaMC.MOD_ID,name), item);
@@ -26,6 +29,11 @@ public class ModItems {
             entries.add(PINK_GARNET);
             entries.add(RAW_PINK_GARNET);
         });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+            entries.add(BANANA);
+        });
+
 
         FuelRegistry.INSTANCE.add(PINK_GARNET, 20*20);
 
